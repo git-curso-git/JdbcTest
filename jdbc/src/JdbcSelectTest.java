765df7749ab3ec85749e1aad1da2542a92b6463b
GIT CURSO GIT <git.curso.git@github.com>
2019-07-07 21:16:21 +0200

    Inicio

---
+++ jdbc/src/JdbcSelectTest.java
@@ -0,0 +1,60 @@
+import java.sql.*;  // Using 'Connection', 'Statement' and 'ResultSet' classes in java.sql package
+ 
+public class JdbcSelectTest {   // Save as "JdbcSelectTest.java"
+	
+	
+	
+	public enum Suit { 
+        DIAMONDS (1, "Diamonds"), 
+        CLUBS    (2, "Clubs"   ), 
+        HEARTS   (3, "Hearts"  ), 
+        SPADES   (4, "Spades"  );
+        
+        private final int value;
+        private final String text;
+        Suit(int value, String text) {
+            this.value = value;
+            this.text = text;
+        }
+        public int value() {return value;}
+        public String text() {return text;}
+    }
+	
+	
+   public static void main(String[] args) {
+      try (
+         // Step 1: Allocate a database 'Connection' object
+         Connection conn = DriverManager.getConnection(
+        		 "jdbc:h2:file:c:/USERS/USER/DATA/TestDataBase", 
+        		 "sa", "" );
+//               "jdbc:mysql://localhost:3306/ebookshop?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+//               "myuser", "xxxx");   // For MySQL only
+               // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"
+ 
+         // Step 2: Allocate a 'Statement' object in the Connection
+         Statement stmt = conn.createStatement();
+      ) {
+         // Step 3: Execute a SQL SELECT query. The query result is returned in a 'ResultSet' object.
+         String strSelect = "select title, price, qty from books";
+         System.out.println("The SQL statement is: " + strSelect + "\n"); // Echo For debugging
+ 
+         ResultSet rset = stmt.executeQuery(strSelect);
+ 
+         // Step 4: Process the ResultSet by scrolling the cursor forward via next().
+         //  For each row, retrieve the contents of the cells with getXxx(columnName).
+         System.out.println("The records selected are:");
+         int rowCount = 0;
+         while(rset.next()) {   // Move the cursor to the next row, return false if no more row
+            String title = rset.getString("title");
+            double price = rset.getDouble("price");
+            int    qty   = rset.getInt("qty");
+            System.out.println(title + ", " + price + ", " + qty);
+            ++rowCount;
+         }
+         System.out.println("Total number of records = " + rowCount);
+ 
+      } catch(SQLException ex) {
+         ex.printStackTrace();
+      }  // Step 5: Close conn and stmt - Done automatically by try-with-resources (JDK 7)
+   }
+}
